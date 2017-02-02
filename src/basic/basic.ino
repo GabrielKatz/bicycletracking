30af27dcd63ff20784ea9362456361423f5acbb4
Gabriel Katz <gkatz@gmx.net>
2017-02-02 18:25:15 +0100

    put tiny gps functionality in (not working yet…)

--- src/basic/basic.ino
+++ src/basic/basic.ino
@@ -1,11 +1,18 @@
-#include <rn2xx3.h>
+#include <rn2xx3.h> 
+#include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 #include "keys.h"
-SoftwareSerial mySerial(7, 8); // RX, TX
+SoftwareSerial myLoraSerial(7, 8); // RX, TX
 #define RST  2
+static const int RXPin = 4, TXPin = 3;
+static const uint32_t GPSBaud = 9600;
+// The serial connection to the GPS device
+SoftwareSerial gpsSerial(RXPin, TXPin);
 
+rn2xx3 myLora(myLoraSerial);
 
-rn2xx3 myLora(mySerial);
+// The TinyGPS++ object
+TinyGPSPlus gps;
 
 // Setup routine runs once when you press reset
 void setup() {
@@ -14,7 +21,7 @@ void setup() {
 
   // Open serial communications and wait for port to open:
   Serial.begin(57600);
-  mySerial.begin(9600);
+  myLoraSerial.begin(9600);
   Serial.println("Startup");
 
   // Reset rn2xx3
@@ -35,29 +42,66 @@ void setup() {
   if(!myLora.initABP(devAddr, appSKey, nwkSKey)){
     Serial.println("Could not connect to LoRaWAN network");
   }
-
+  
+  delay(500);
+  Serial.println("Initializing GPS...");
+  gpsSerial.begin(GPSBaud);
+  Serial.println("Initialized GPS");
+  
   led_off();
   delay(2000);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  Serial.println("TXing");
-  myLora.txUncnf("X");
-
-  String response = myLora.getRx();
-  if(response && response.length() > 0) {
-  Serial.println("Response: " + response);
-    if(response.toInt() == 31) {
-      led_on();
+  Serial.println("starting loop...");
+  while (gpsSerial.available() > 0){
+    if (gps.encode(gpsSerial.read())){
+      displayInfo();
+      if (gps.location.isValid()){
+        transmit_coords(gps.location.lat(), gps.location.lng());
+      }
+      else{
+        Serial.println("GPS location not valid");
+      }
     }
-    else{
-      led_off();
+    else {
+      Serial.println("GPS location could not be encoded");
     }
+  }
+  else{
+    Serial.println("No GPS Received");
+  }
+  if(receivedBikeKillSignal()){
+    Serial.println("Deactivating Bike...");
   }
   delay(20000);
 }
 
+boolean receivedBikeKillSignal() {
+  String response = myLora.getRx();
+  if(response && response.length() > 0) {
+    Serial.println("Response: " + response);
+    return response.toInt() == 31;
+  }
+  return false;
+}
+
+void transmit_coords(double float_latitude, double float_longitude){
+  uint8_t coords[6]; // 6*8 bits = 48
+  int32_t lat = float_latitude * 10000;
+  int32_t lon = float_longitude * 10000;
+
+  // Pad 2 int32_t to 6 8uint_t, skipping the last byte (x >> 24)
+  coords[0] = lat;
+  coords[1] = lat >> 8;
+  coords[2] = lat >> 16;
+  
+  coords[3] = lon;
+  coords[4] = lon >> 8;
+  coords[5] = lon >> 16;
+  myLora.txBytes(coords, sizeof(coords));
+}
 void led_on()
 {
   digitalWrite(13, 1);
@@ -66,4 +110,55 @@ void led_on()
 void led_off()
 {
   digitalWrite(13, 0);
+}
+
+void displayInfo()
+{
+  Serial.print(F("Location: "));
+  if (gps.location.isValid())
+  {
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 6);
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+ Serial.print(F("  Date/Time: "));
+  if (gps.date.isValid())
+  {
+    Serial.print(gps.date.month());
+    Serial.print(F("/"));
+    Serial.print(gps.date.day());
+    Serial.print(F("/"));
+    Serial.print(gps.date.year());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+ Serial.print(F(" "));
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.hour());
+    Serial.print(F(":"));
+    if (gps.time.minute() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.minute());
+    Serial.print(F(":"));
+    if (gps.time.second() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.second());
+    Serial.print(F("."));
+    if (gps.time.centisecond() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.centisecond());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+ Serial.println();
 }
