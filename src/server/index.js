d1d936d77619019e17c6a579d4a5143dcc0e0a62
Gabriel Katz <gkatz@gmx.net>
2017-02-02 21:43:28 +0100

    read out coordinates.

--- src/server/index.js
+++ src/server/index.js
@@ -2,9 +2,15 @@ let ttn = require('ttn');
 let accessKey = require('./keys').accessKey;
 let appId = 'biketheftmakezurich'
 
+let locationByDevice = {};
+
 console.log('access key: ' + accessKey);
 let client = new ttn.Client('eu', appId, accessKey);
 
 client.on('message', function(deviceId, message) {
-  console.log(message.payload_raw.toString('ascii'));
+
+  let location = message.payload_fields.location
+  console.log(`Received location from device ${deviceId}. Latitude: ${location.lat}, Longitude: ${location.lng}`);
+  locationByDevice[deviceId] = location;
+  
 });
