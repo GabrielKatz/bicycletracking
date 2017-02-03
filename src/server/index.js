b63b93199eeb9d18df5792b5470cdad1c0356c7c
Gabriel Katz <gkatz@gmx.net>
2017-02-03 16:55:34 +0100

    load position data from store

--- src/server/index.js
+++ src/server/index.js
@@ -1,24 +1,44 @@
-let ttn = require('ttn');
 let accessKey = require('./keys').accessKey;
 let appId = 'biketheftmakezurich';
 let express = require('express');
 let open = require('open');
-let locationByDevice = {};
-
-console.log('access key: ' + accessKey);
-let client = new ttn.Client('eu', appId, accessKey);
-
-client.on('message', (deviceId, message) => {
-  let location = message.payload_fields.location
-  console.log(`Received location from device ${deviceId}. Latitude: ${location.lat}, Longitude: ${location.lng}`);
-  locationByDevice[deviceId] = location;
-});
+let request = require('request');
+let path = require('path');
 
 const port = 3000;
 const app = express();
-app.get('*', (req, res) => {
-  res.send(locationByDevice);
+app.use(express.static('static'));
+app.get('/coords/', (req, res) => {
+  request(`https://${appId}.data.thethingsnetwork.org/api/v2/query?last=1d`, {
+    method: "GET",
+    headers: {
+      'Authorization': 'key ' + accessKey
+    }
+  }, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+
+      let positionsByDevice = JSON.parse(body).filter(el => el.lat && el.lng).reduce((prev, message) => {
+        let timeObject = {
+          time: message.time,
+          lat: message.lat,
+          lng: message.lng
+        }
+        if(message.device_id in prev) {
+          prev[message.device_id].push(timeObject);
+        }
+        else {
+          prev[message.device_id] = [timeObject];
+        }
+        return prev;
+      }, {});
+      res.send(positionsByDevice);
+    }
+  });
 });
+app.get('/', (req,res ) => {
+  res.sendFile(path.join( __dirname, '/static/biketracking.html'));
+})
+
 
 app.post('shutdown/:id', (req, res) => {
   const deviceId = req.params.id;
