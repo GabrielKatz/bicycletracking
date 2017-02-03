d241ae0446aedc79a3520cdff529f87c680ed448
Gabriel Katz <gkatz@gmx.net>
2017-02-03 12:13:46 +0100

    added express server to return coordinates

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,28 @@
 let ttn = require('ttn');
 let accessKey = require('./keys').accessKey;
-let appId = 'biketheftmakezurich'
-
+let appId = 'biketheftmakezurich';
+let express = require('express');
+let open = require('open');
 let locationByDevice = {};
 
 console.log('access key: ' + accessKey);
 let client = new ttn.Client('eu', appId, accessKey);
 
-client.on('message', function(deviceId, message) {
-
+client.on('message', (deviceId, message) => {
   let location = message.payload_fields.location
   console.log(`Received location from device ${deviceId}. Latitude: ${location.lat}, Longitude: ${location.lng}`);
   locationByDevice[deviceId] = location;
-  
+});
+
+const port = 3000;
+const app = express();
+app.get('*', (req, res) => {
+  res.send(locationByDevice);
+});
+app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    open(`http://localhost:${port}`);
+  }
 });
