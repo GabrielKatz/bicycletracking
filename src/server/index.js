b4d3efea4513bd750594a67ffda787d221235d28
Gabriel Katz <gkatz@gmx.net>
2017-02-04 12:12:48 +0100

    bikes route for mmmatthew’s view.

--- src/server/index.js
+++ src/server/index.js
@@ -39,6 +39,38 @@ app.get('/coords/', (req, res) => {
     }
   });
 });
+app.get('/bikes/', (req, res) => {
+  request(`https://${appId}.data.thethingsnetwork.org/api/v2/query?last=1d`, {
+    method: "GET",
+    headers: {
+      'Authorization': 'key ' + accessKey
+    }
+  }, (error, response, body) => {
+    if (error || response.statusCode !== 200) {
+      res.sendStatus(500);
+      return;
+    }
+    let deviceInfos = JSON.parse(body).filter(el => el.lat && el.lng).reduce((prev, message) => {
+      let timeObject = {
+        id: message.device_id,
+        status: 'ok',
+        type: 'm-bike',
+        lastSeen: message.time,
+        coords: [message.lat, message.lng]
+      };
+      if(!prev[message.deviceId] ||
+         prev[message.device_id].lastSeen < timeObject.lastSeen) {
+        prev[message.device_id] = timeObject;
+      }
+      return prev;
+    }, {});
+    let result = [];
+    for(let deviceInfo in deviceInfos){
+      result.push(deviceInfos[deviceInfo]);
+    }
+    res.send(result);
+  });
+});
 app.get('/', (req,res ) => {
   res.sendFile(path.join( __dirname, '/static/biketracking.html'));
 })
